2bb351fb27523e1ca1e32f57052e4cd9490f4bd6
Zhijia Zhu <zhuzhijia@mail.ustc.edu.cn>
2023-06-05 18:33:54 +0800

    2023.06.05 Updated and optimized AlexNet's annotations, and completed the experiment for the 2nd time.

--- pytorch_classification/Test2_alexnet/train.py
+++ pytorch_classification/Test2_alexnet/train.py
@@ -17,15 +17,26 @@ from model import AlexNet
 
 def main():
     # torch.device函数：指定我们使用的设备，默认使用GPU
+    """
+    divice： device类的实例
+    """
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     # 格式化字符串的函数 str.format()
     print("using {} device.".format(device))
 
     # 定义数据预处理函数
     # 对于训练集：
-    # RandomResizedCrop：随机裁剪 224*224 RandomHorizontalFlip：随机翻转 ToTensor：转换为tensor Normalize：标准化处理
+        # RandomResizedCrop：随机裁剪 224*224
+        # RandomHorizontalFlip：随机翻转
+        # ToTensor：转换为
+        # tensor Normalize：标准化处理
     # 对于验证集：
-    # Resize：224*224
+        # Resize：224*224
+    """
+    data_transform: dict:2
+        train: Compose实例，下面是4个列表，每个列表的元素是一个对应预处理方法的类的实例
+        val: 同理
+    """
     data_transform = {
         # torchvision.transforms：图像转换类
         # 字典
@@ -41,18 +52,27 @@ def main():
     # os.path.join：把目录和文件名合成一个路径
     # os.getcwd() 方法用于返回当前工作目录
     # "../.." 返回上上层目录，即将当前工作目录和上上层目录合并起来
+    """
+    data_root str
+    """
     data_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))  # get data root path
     # 图片地址：data_root+data_set+flower_Data 路径
     # 即：E:\RCNN\deep-learning-for-image-processing-master\data_set\flower_data
+    """
+    image_path str
+    """
     image_path = os.path.join(data_root, "data_set", "flower_data")  # flower data set path
 
     # os.path.exists(path) 用来判断 path 参数所指的文件或文件夹是否存在
     # assert：断言
     assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
-
     # ImageFolder：载入训练集
     # root：图片的根目录，所在目录的上一级目录
     # transform：对图片进行预处理的操作（函数），原始图片作为输入，返回一个转换后的图片。
+    """
+    train_dataset: ImageFolder: 3306
+    ImageFolder 是 pytorch中假定图片按类别分类的加载类
+    """
     train_dataset = datasets.ImageFolder(root=os.path.join(image_path, "train"),
                                          transform=data_transform["train"])
     # 通过len函数打印训练集有多少张图片
@@ -60,14 +80,23 @@ def main():
 
     # {'daisy':0, 'dandelion':1, 'roses':2, 'sunflower':3, 'tulips':4}
     # train_dataset.class_to_idx：获取分类的名称所对应的索引
+    """
+    flower_list: dict -> 0: daisy
+    """
     flower_list = train_dataset.class_to_idx
     # 遍历字典，让key val返过来
     # 目的：返回的索引就可以通过字典得到对应的类别
+    """
+    cla_dict: dict -> daisy: 0
+    """
     cla_dict = dict((val, key) for key, val in flower_list.items())
     # write dict into json file
     # 通过json包将cla_dict字典编码成json格式
+    """
+    json_str: str
+    """
     json_str = json.dumps(cla_dict, indent=4)
-    # 打开'class_indices.json'文件，
+    # 打开'class_indices.json'文件，写入json_str
     with open('class_indices.json', 'w') as json_file:
         json_file.write(json_str)
 
@@ -76,10 +105,17 @@ def main():
     print('Using {} dataloader workers every process'.format(nw))
     # data.DataLoader函数将【训练集】train_dataset 加载进来，通过给定的batch_size和随机参数，能够从样本中获得一批一批数据
     # num_workers：加载我们数据所使用的线程个数，0：主线程
+    """
+    train_loader: DataLoader类的实例：104个数据加载器， 3306/32
+    """
     train_loader = torch.utils.data.DataLoader(train_dataset,
                                                batch_size=batch_size, shuffle=True,
                                                num_workers=0)
     # ImageFolder：载入【测试集】，transform：返回测试集的预处理方法
+    """
+    validate_dataset: ImageFolder: 3306
+    ImageFolder 是 pytorch中假定图片按类别分类的加载类
+    """
     validate_dataset = datasets.ImageFolder(root=os.path.join(image_path, "val"),
                                             transform=data_transform["val"])
     # 通过len函数打印测试集有多少张图片
@@ -87,6 +123,9 @@ def main():
     # torch.utils.data.DataLoader：数据加载器，结合了数据集和取样器，并且可以提供多个线程处理数据集。
     # dataset：输入的数据
     # shuffle:洗牌（如果数据是有序列特征的，就不要设置成True）
+    """
+    validate_loader: DataLoader类的实例：91个数据加载器， batchsize = 4: 4*91
+    """
     validate_loader = torch.utils.data.DataLoader(validate_dataset,
                                                   batch_size=4, shuffle=True,
                                                   num_workers=0)
@@ -107,15 +146,24 @@ def main():
     # imshow(utils.make_grid(test_image))
 
     # 参数类别=5，初始化权重=true
+    """
+    AlexNet类的实例
+    """
     net = AlexNet(num_classes=5, init_weights=True)
     # 指定设备
     net.to(device)
     # 定义损失函数：多类别的损失交叉熵
+    """
+    CrossEntropyLoss: CrossEntropy类的实例
+    """
     loss_function = nn.CrossEntropyLoss()
     # 调试时使用，用来查看模型的参数
     # list() 方法用于将元组转换为列表。
     # pata = list(net.parameters())
     # 定义了一个Adam优化器，对象：网络中所有可训练的参数，学习率：0.0002
+    """
+    Adam类的实例
+    """
     optimizer = optim.Adam(net.parameters(), lr=0.0002)
 
     epochs = 10
@@ -123,6 +171,8 @@ def main():
     save_path = './AlexNet.pth'
     # 定义了：最佳准确率，保存准确率最高的一次模型
     best_acc = 0.0
+    """
+    """
     train_steps = len(train_loader)
 
     # 开始【训练】，迭代10次
@@ -135,15 +185,28 @@ def main():
         # 训练开始-训练结束：得到训练时间
         t1 = time.perf_counter()
         # tqdm：在长循环中添加一个进度条
+        """
+        将加载器传入tqdm
+        共生成 104 个 tqdm实例 -> 包装后的加载器
+        
+        """
         train_bar = tqdm(train_loader)
 
-        # 遍历数据集
+        # 遍历数据集(遍历数据里的加载器)
         for step, data in enumerate(train_bar):
             # 分为图像、标签
+            """
+            images: Tensor [32, 3, 224, 224]
+            labels: Tensor [32]
+            """
             images, labels = data
             # 清空梯度信息
             optimizer.zero_grad()
             # 进行正向传播，将训练图像指认到设备当中，得到输出
+            """
+            outputs: Tensor [32, 5]
+            [-0.0478, -0.0749, -0.0314,  0.0435, -0.0485]
+            """
             outputs = net(images.to(device))
             # 利用lose_function来计算预测值和真实值的损失（将lable指认到设备当中）
             loss = loss_function(outputs, labels.to(device))
@@ -152,11 +215,14 @@ def main():
             # 通过optimizer更新每一个结点的参数
             optimizer.step()
             # print statistics，将loss的值累加到running_loss当中
+            """
+            142.08087539672852
+            """
             running_loss += loss.item()
 
         train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,
-                                                                     epochs,
-                                                                     loss)
+                                                                 epochs,
+                                                                 loss)
 
         # validate 训练完一轮后进行【验证】
         net.eval()
@@ -186,7 +252,6 @@ def main():
             torch.save(net.state_dict(), save_path)
 
     print('Finished Training')
-
 
 
 if __name__ == '__main__':
